05fa17738780004cf220acea6af9e83a1182a330
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2024-11-04 19:34:45 +0000

    Update allchat.py

--- allchat.py
+++ allchat.py
@@ -167,3 +167,5 @@ elif st.session_state['authentication_status'] is False:
     st.error('Username/password is incorrect')
 elif st.session_state['authentication_status'] is None:
     st.warning('Please enter your username and password')
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
